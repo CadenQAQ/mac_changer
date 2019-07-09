e5c5663ad1c28c80fc31f72a2387dce0163b5550
root <cadencityu@gmail.com>
2019-07-09 13:03:20 -0400

    create def mac_changer,

--- mac_changer.py
+++ mac_changer.py
@@ -10,7 +10,7 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-parser = optparse.OptionParser()  # OptionParser is a class
+parser = optparse.OptionParser()  # OptionParser is a class, and parser is an instance
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
 parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
