3c5f8c5fe1979293df147a73a5733b52c09b2e44
root <root@localhost.localdomain>
2019-07-09 12:58:36 -0400

    Initial commit

---
+++ mac_changer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import subprocess
+import optparse
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+parser = optparse.OptionParser()  # OptionParser is a class
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+(option, arguments) = parser.parse_args()
+
+change_mac(option.interface, option.new_mac)
+
