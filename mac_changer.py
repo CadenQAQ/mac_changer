935256c0d28650719a7ce0f5539de5a106a10aa0
root <cadencityu@gmail.com>
2019-07-10 12:15:47 -0400

    create functions to handle the input,

--- mac_changer.py
+++ mac_changer.py
@@ -3,6 +3,21 @@
 import subprocess
 import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()  # OptionParser is a class, and parser is an instance
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface:
+        parser.error("Please specify an interface, use --help for more info.")  # if the user does not input the int
+    elif not options.new_mac:
+        parser.error("Please specify a new mac, use --help for more info.") # if the user does not input the mac
+
+    return options
+
+
 def change_mac(interface, new_mac):
     print("[+] Changing MAC Address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
@@ -10,11 +25,7 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-parser = optparse.OptionParser()  # OptionParser is a class, and parser is an instance
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
-(option, arguments) = parser.parse_args()
-
-change_mac(option.interface, option.new_mac)
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
 
